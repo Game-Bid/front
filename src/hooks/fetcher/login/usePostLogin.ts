1c0ada0f90f160e4547d10cb57b5d0c2fd8f213c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-04-22 04:33:03 +0900

    feat:로그인API연결

---
+++ src/hooks/fetcher/login/usePostLogin.ts
@@ -0,0 +1,21 @@
+import { showToast } from "@/components/common/Toast";
+import { postLogin, PostLoginData } from "@/services/login/postLogin";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation"
+
+export const usePostLogin = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: (formData: PostLoginData) => postLogin(formData),
+    onSuccess: () => {
+      router.push("/");
+      showToast(
+        "success", "로그인 성공", "환영합니다! 정상적으로 로그인되었어요."
+      );
+    },
+    onError: () => {
+      showToast("warning", "로그인 정보를 확인해주세요.", "이메일 또는 비밀번호가 올바르지 않습니다.");
+    }
+  })
+}
---
+++ src/services/login/postLogin.ts
@@ -0,0 +1,30 @@
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export interface PostLoginData {
+  email: string;
+  password: string;
+}
+
+export const postLogin = async (formData: PostLoginData) => {
+  try {
+    const res = await fetch(`${apiUrl}/api/v1/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    });
+
+    if (!res.ok) {
+      const errorData = await res.text();
+      throw new Error(errorData || "로그인 응답 에러");
+    }
+
+    const result = await res.text();
+    return result;
+  } catch (err) {
+    console.log("Error details:", err);
+    throw err instanceof Error ? err : new Error("알 수 없는 에러");
+  }
+}
