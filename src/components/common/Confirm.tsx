9b0ce0455264db1b4c0932e2d1add6e55f8834b1
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2025-05-11 00:21:34 +0900

    style:confirm,alert 스타일 변경

--- src/components/common/Confirm.tsx
+++ src/components/common/Confirm.tsx
@@ -37,18 +37,18 @@ const Confirm = ({
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
-            className="w-[400px] rounded-lg bg-bgGrayDepth3 p-24 flex-center flex-col gap-24"
+            className="w-[400px] tablet:w-[300px] rounded-lg bg-bgGrayDepth3 p-1.5 flex-center flex-col gap-1.5"
           >
-            <div className="flex-center flex-col gap-16">
+            <div className="flex-center flex-col gap-1">
               {icon && (
-                <div className="rounded-max bg-fillGrayFocused w-[48px] h-[48px] flex-center">
+                <div className="rounded-max bg-fillGrayFocused w-[48px] h-[48px] tablet:w-[36px] tablet:h-[36px] flex-center">
                   <CustomIcon
                     icon={customIcon as IconList}
-                    className="w-[24px] h-[24px]"
+                    className="w-[24px] h-[24px] tablet:w-[18px] tablet:h-[18px]"
                   />
                 </div>
               )}
-              <div className="flex-center flex-col gap-4 leading-[1.4]">
+              <div className="flex-center flex-col gap-0.25 leading-[1.4]">
                 <div
                   className="font-semibold text-fgGrayDefault text-[1.125rem] tracking-[-0.36px]
   "
@@ -61,17 +61,15 @@ const Confirm = ({
               </div>
             </div>
 
-            <div className="flex gap-4 justify-between w-full">
+            <div className="flex gap-0.5 justify-between w-full">
               <Button
                 title={falseButtonTitle}
-                size="md"
                 variant={"secondary"}
                 onClick={falseSubmit}
                 width="100%"
               />
               <Button
                 title={trueButtonTitle}
-                size="md"
                 variant={"primary"}
                 onClick={trueSubmit}
                 width="100%"
