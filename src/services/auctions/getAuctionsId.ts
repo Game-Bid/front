30a895d0c291ca39d2df9cd0c47a79474b3c616b
chaeyoung <call0512@naver.com>
2025-05-13 22:47:53 +0900

    feat:경매페이지 신규 ui 개발 (#72)

---
+++ src/services/auctions/getAuctionsId.ts
@@ -0,0 +1,26 @@
+"use server";
+
+const apiUrl = process.env.NEXT_API_URL;
+
+export const getAuctionsId = async (id: string) => {
+  console.log(`${apiUrl}/api/v1/auctions/${id}?increment=false`);
+  try {
+    const res = await fetch(`${apiUrl}/api/v1/auctions/${id}?increment=false`);
+
+    if (!res.ok) {
+      return {
+        error: "auction id Api 응답 에러",
+        status: res.status,
+        statusText: res.statusText,
+      };
+    }
+    const result = await res.json();
+
+    console.log(result);
+
+    return { result };
+  } catch (err) {
+    console.log("Error details:", err);
+    return { error: "game API 요청 실패", errorMessage: err };
+  }
+};
