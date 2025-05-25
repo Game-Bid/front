698fb303264cfd16d521283dde8417dd9d034964
jiyoon <ljy10499@gmail.com>
2025-05-25 17:49:51 +0900

    features: 빌드 오류 수정

--- src/app/my-page/payments/page.tsx
+++ src/app/my-page/payments/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {Suspense} from "react";
 import MyPagePaymentsTop from "@/components/my-page/payments/MyPagePaymentsTop";
 
 
 const MyPagePaymentPage = () => {
 
     return <div className={'w-[793px] flex flex-col gap-l-2 flex-start self-stretch font-semibold text-fgGrayDefault'}>
-        <MyPagePaymentsTop/>
+        <Suspense>
+            <MyPagePaymentsTop/>
+        </Suspense>
     </div>;
 };
 
