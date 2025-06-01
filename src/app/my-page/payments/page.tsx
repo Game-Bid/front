e2c3bb36c6750321a9df177ab1cbe80c3db1c36d
jiyoon <ljy10499@gmail.com>
2025-06-01 13:39:56 +0900

    features: empty 시 화면 처리

--- src/app/my-page/payments/page.tsx
+++ src/app/my-page/payments/page.tsx
@@ -1,13 +1,24 @@
 import React, {Suspense} from "react";
 import MyPagePaymentsTop from "@/components/my-page/payments/MyPagePaymentsTop";
+import PaymentWaitingItem from "@/components/my-page/payments/PaymentWaitingItem";
 
 
 const MyPagePaymentPage = () => {
+    const data = [1];
 
     return <div className={'w-[793px] flex flex-col gap-l-2 flex-start self-stretch font-semibold text-fgGrayDefault'}>
         <Suspense>
             <MyPagePaymentsTop/>
         </Suspense>
+        {data.length <= 0 ?
+            <div
+                className={'flex h-[256px] justify-center items-center self-stretch font-normal text-0.875 leading-[1.4] rounded-lg bg-bgGrayDepth2'}>
+                결제 대기중인 경매가 없습니다
+            </div>
+            : <div>
+                <PaymentWaitingItem/>
+            </div>}
+
     </div>;
 };
 
