2ce1d10baa9756dfdde33e66e6566e28570de832
jiyoon <ljy10499@gmail.com>
2025-06-18 16:10:08 +0900

    feature: 검색 컴포넌트 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import React from "react";
-import Pagination from "@/components/common/Pagination";
-import SearchBox from "@/components/main/SearchBox";
+import {redirect} from "next/navigation";
 
 const Page = () => {
-  return  <div className={'h-full w-screen'}>
-    <div className="flex flex-col flex-center  w-full max-w-[1200px] m-auto ">
-      <SearchBox />
-      <Pagination lastPage={20}/>
-    </div>
-  </div>;
+    redirect('/main');
+    return null;
 };
 
 export default Page;
